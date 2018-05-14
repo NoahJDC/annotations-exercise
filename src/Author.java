99b722c10bccca3a807de9c9b6e897a9d5b05961
Noah Dela Cruz <noahjdc92@gmai.com>
2018-05-14 15:53:32 -0500

    made fix better by adding list and string

--- src/Author.java
+++ src/Author.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<String>();
     }
 
     /**
@@ -27,7 +27,6 @@ public class Author extends Person {
     public void addBook(String book) {
         books.add(book);
     }
-
 
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
